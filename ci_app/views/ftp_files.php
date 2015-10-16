264b4ace1b4dff43faf2557e0f5ae12a77c59e0c
Clockwork <clockwork.0509@gmail.com>
2015-10-16 14:30:00 +0800

    put <div class=container>

--- ci_app/views/ftp_files.php
+++ ci_app/views/ftp_files.php
@@ -2,13 +2,15 @@
 <link rel="stylesheet" href="<?php echo site_url("../assets/css/treeview/treeview.css"); ?>"/>
 
 <div class="row">
-	<div class="page-header">
-		<h1><a class="label label-success" href="http://www.veteransbank.com.ph" target="_blank">PVB</a> <small>Philippine Veterans Bank</small></h1>
-	</div>
-	<div class="well">
-		<legend>Board Director Files</legend>
-		<div class="css-treeview">
-		<?php echo $ulli; ?>
+	<div class="col-sm-offset-1 col-sm-10 col-md-offset-0 col-md-12">
+		<div class="page-header">
+			<h1><a href="http://www.veteransbank.com.ph" target="_blank"><img src="<?php echo site_url("/PVBIcon4.ico") ?>"></img></a> <small>Philippine Veterans Bank</small></h1>
+		</div>
+		<div class="well">
+			<legend>Board Director Files</legend>
+			<div class="css-treeview">
+			<?php echo $ulli; ?>
+			</div>
 		</div>
 	</div>
 </div>
--- ci_app/views/templates/footer.php
+++ ci_app/views/templates/footer.php
@@ -1,17 +1,21 @@
 <?php if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 		<footer class="footer">
 		  <div class="container">
-			<center>
-			<p class="label">
-				<a href="http://www.veteransbank.com.ph" target="_blank">PVB Official Website</a> <span class="text-info">|</span> 
-				<a href="http://www.veteransbank.com.ph/Vol1%20Issue4%20072307.pdf" target="_blank">Acquired Assets</a> <span class="text-info">|</span> 
-				<a href="http://192.168.50.189/suggestion.asp"  target="_blank">Suggestion Box</a>
-			</p>
-			<br/>
-			<div class="label label-default">
-				Copyright <span class="glyphicon glyphicon-copyright-mark" aria-hidden="true"></span> 2015 PVB Philippine Veterans Bank. All rights reserved.
-			</div>
-			</center>
+			<div class="row">
+				<div class="col-sm-offset-1 col-sm-10 col-md-offset-0 col-md-12">
+					<center>
+						<p class="label">
+							<a href="http://www.veteransbank.com.ph" target="_blank">PVB Official Website</a> <span class="text-info">|</span> 
+							<a href="http://www.veteransbank.com.ph/Vol1%20Issue4%20072307.pdf" target="_blank">Acquired Assets</a> <span class="text-info">|</span> 
+							<a href="http://192.168.50.189/suggestion.asp"  target="_blank">Suggestion Box</a>
+						</p>
+						<br/>
+						<div class="label label-default">
+							Copyright <span class="glyphicon glyphicon-copyright-mark" aria-hidden="true"></span> 2015 PVB Philippine Veterans Bank. All rights reserved.
+						</div>
+					</center>
+				</div>
+			<div>
 		  </div>
 		</footer>
 	</div>
